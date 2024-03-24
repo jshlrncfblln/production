a93a2ac19ffd11952af0c9c8e9bf217fce3848f6
LYKAMAEEE <133262545+LYKAMAEEE@users.noreply.github.com>
2024-03-25 03:33:44 +0800

    update

--- balaifinder-app/src/components/navbar.jsx
+++ balaifinder-app/src/components/navbar.jsx
@@ -284,22 +284,8 @@ function Navbar() {
                 <li>
                   <button
                     onClick={togglePopMenu}
-                    className="text-gray-300 hover:text-white focus:outline-none focus:text-white"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16m-7 6h7"
-                      />
-                    </svg>
+                    className="rounded-md px-2 py-2 hover:shadow-md">
+                      <FaUser />
                   </button>
                   <Transition
                       show={isOpenPopMenu}
